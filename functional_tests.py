6723e07c6d88bb20467be655697e7546e1844cc9
Sam Bozek <sambozek@gmail.com>
2021-12-01 15:01:00 -0800

    Functional test checks input of a chemical

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,11 +17,23 @@ class NewVisitorTest(unittest.TestCase):
 
         # Page title includes Chemical Inventory
         self.assertIn('Chemical Inventory', self.browser.title)
-        self.fail('Complete the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Chemical Inventory', header_text)
 
         # Invited to add a new chemical
         # Adds Hexanes (CAS #110-54-3)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter Chemical')
 
+        inputbox.send_keys('Hexanes')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Hexanes' for row in rows))
+
+        self.fail("Finish The Test!")
         # Inputs the receipt date
 
         # Leaves open date blank
@@ -31,6 +45,8 @@ class NewVisitorTest(unittest.TestCase):
         # Adds Lot Number
 
         # Scans Barcode
+
+        # Marks chemical as used up
 
 if __name__ == '__main__':
     unittest.main()

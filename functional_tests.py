4a96d19849aa2fcf6d3cd95865607fc06a1b9e01
Sam Bozek <sambozek@gmail.com>
2021-12-01 13:17:45 -0800

    Modified FT to use unittest, created first FT for page title

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,36 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Congratulations!' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_input_inventory_and_retrieve_it_later(self):
+        # The page for the inventory management system
+        self.browser.get('http://localhost:8000')
+
+        # Page title includes Chemical Inventory
+        self.assertIn('Chemical Inventory', self.browser.title)
+        self.fail('Complete the test!')
+
+        # Invited to add a new chemical
+        # Adds Hexanes (CAS #110-54-3)
+
+        # Inputs the receipt date
+
+        # Leaves open date blank
+
+        # Adds Volume
+
+        # Adds Supplier Name
+
+        # Adds Lot Number
+
+        # Scans Barcode
+
+if __name__ == '__main__':
+    unittest.main()

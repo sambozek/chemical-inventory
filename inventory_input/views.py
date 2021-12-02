0f7da1b51490ec7a104dcdc2ed3a61a09e0a501b
Sam Bozek <sambozek@gmail.com>
2021-12-01 16:09:05 -0800

    Refactored homepage to use a template

--- inventory_input/views.py
+++ inventory_input/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-    return HttpResponse('<html><title>Chemical Inventory</title></html>')
+    return render(request, 'home.html')

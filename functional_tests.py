64c7ff01a2e90fb5b2ac99a6a3306e067cdebcf7
Sam Bozek <sambozek@gmail.com>
2021-12-03 17:22:33 -0800

    Quick addition of a supporting test.

--- functional_tests.py
+++ functional_tests.py
@@ -8,8 +8,13 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
     
-    def tearDown(self) -> None:
+    def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_input_inventory_and_retrieve_it_later(self):
         # The page for the inventory management system
@@ -28,11 +33,15 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Hexanes')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('Hexanes')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Hexanes' for row in rows),
-        "New chemical not in table")
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Acetone')
+        inputbox.send_keys
+        time.sleep(1)
+
+        self.check_for_row_in_list_table('Hexanes')
+        self.check_for_row_in_list_table('Acetone')
 
         self.fail("Finish The Test!")
         # Inputs the receipt date

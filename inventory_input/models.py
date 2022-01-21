404b0d96240d9f4b277a1f012520708e47d6f60f
Sam Bozek <sambozek@gmail.com>
2022-01-21 13:12:05 -0800

    Model for the inventory items with migrations for them

--- inventory_input/models.py
+++ inventory_input/models.py
@@ -1,3 +1,7 @@
+from operator import mod
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    text = models.TextField(default='')
--- inventory_input/tests.py
+++ inventory_input/tests.py
@@ -1,13 +1,14 @@
-from django import http
+# from django import http
 from django.test import TestCase
-from django.urls import resolve
-from django.http import HttpRequest, response
-from django.template.loader import render_to_string
+# from django.urls import resolve
+# from django.http import HttpRequest, response
+# from django.template.loader import render_to_string
 from inventory_input.models import Item
 
-from inventory_input.views import home_page
-
+#  from inventory_input.views import home_page
 # Create your tests here.
+
+
 class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
@@ -20,4 +21,24 @@ class HomePageTest(TestCase):
         })
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) inv item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Inv the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) inv item')
+        self.assertEqual(second_saved_item.text, 'Inv the second')
 

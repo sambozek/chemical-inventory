916a9eba59aef6a69cf9bbbf812d0aeb9d31dd69
Sam Bozek <sambozek@gmail.com>
2022-01-26 15:57:10 -0800

    removed the unneeded waits

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 from django.test import LiveServerTestCase
 import time
 
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -12,10 +14,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_input_inventory_and_retrieve_it_later(self):
         # The page for the inventory management system
@@ -34,15 +44,15 @@ class NewVisitorTest(LiveServerTestCase):
 
         inputbox.send_keys('Hexanes')
         inputbox.send_keys(Keys.ENTER)
-
-        time.sleep(1)
+        self.wait_for_row_in_list_table('Hexanes')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
 
         inputbox.send_keys('Acetone')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('Hexanes')
+
+        self.wait_for_row_in_list_table('Acetone')
+        self.wait_for_row_in_list_table('Hexanes')
 
         self.fail("Finish The Test!")
         # Inputs the receipt date
